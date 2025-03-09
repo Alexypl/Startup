d09cf770ce53b19eaff5e83024ba1256fc2f4490
Alexypl <51360612+Alexypl@users.noreply.github.com>
2025-03-09 11:05:10 +0100

    Create 0_Table_configuration.sql

---
+++ SQL/0_Table_configuration.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS startup (
+StartupName varchar(50),
+Industry varchar(50),
+FundingRounds int,
+FundingAmount float,
+Valuation float,
+Revenue float,
+Employees int,
+MarketShare float,
+Profitable int,
+YearFounded int,
+Region varchar(50),
+ExitStatus varchar(50)
+)
