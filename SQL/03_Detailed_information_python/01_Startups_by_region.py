4310a6e9f34d15f6e8d96cc4f490ed27b3baf329
Alexypl <51360612+Alexypl@users.noreply.github.com>
2025-04-22 22:40:00 +0200

    main

---
+++ SQL/03_Detailed_information_python/01_Startups_by_region.py
@@ -0,0 +1,6 @@
+startups_by_region = startup.groupby('Region')[['StartupName']].count().reset_index()\
+.sort_values(by = 'StartupName', ascending = False)
+
+sns.barplot(data = startups_by_region, x = 'Region', y = 'StartupName')
+plt.xticks(rotation = 45)
+plt.show()
