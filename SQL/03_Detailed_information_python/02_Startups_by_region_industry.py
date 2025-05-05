134a5f8b1e0d70e952a6d8b78d9d4a54b8d2df79
Alexypl <51360612+Alexypl@users.noreply.github.com>
2025-05-06 00:07:47 +0200

    main

---
+++ SQL/03_Detailed_information_python/02_Startups_by_region_industry.py
@@ -0,0 +1,10 @@
+startups_by_region_industry = startup.groupby(['Region', 'Industry'])[['StartupName']].count().reset_index()
+
+startups_by_region_industry = startups_by_region_industry.sort_values(by = ['Region', 'StartupName'], ascending = [True, False])
+
+sns.barplot(data = startups_by_region_industry, x = 'Region', y = 'StartupName', hue = 'Industry')
+plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
+plt.xticks(rotation = 45)
+plt.yticks(ticks = [i for i in range(0, 21, 5)], labels=[i for i in range(0, 21, 5)])
+plt.savefig("03_startups_by_region_industry.png", bbox_inches='tight')
+plt.show()
